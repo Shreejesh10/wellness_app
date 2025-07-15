9ec4115b41e398478610b408598773eff9605774
Shreejesh Pathak <shreejeshpathak@gmail.com>
2025-07-15 21:45:45 +0545

    Have completed quotes page and profile section and made enhanancement on preference

--- lib/core/route_config/route_config.dart
+++ lib/core/route_config/route_config.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wellness_app/core/route_config/route_names.dart';
+import 'package:wellness_app/preference.dart';
+import 'package:wellness_app/profile.dart';
+import 'package:wellness_app/quotes.dart';
 import '../../features/auth/login.dart';
 import '../../features/auth/signup.dart';
 import '../../features/dashboard/dashboard.dart';
@@ -27,6 +30,18 @@ class RouteConfig {
       case AuthRouteName.signupScreen:
         return MaterialPageRoute(
           builder: (_) => const SignupScreen(),
+        );
+      case RoutesName.preferenceScreen:
+        return MaterialPageRoute(
+            builder: (_) => UserPreferenceScreen()
+        );
+      case RoutesName.profileScreen:
+        return MaterialPageRoute(
+            builder: (_) => const UserProfileScreen()
+        );
+      case RoutesName.quotesScreen:
+        return MaterialPageRoute(
+            builder: (_) => QuotesScreen()
         );
         default:
           return _errorRoute();
